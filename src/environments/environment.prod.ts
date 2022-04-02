d148a46a7b27ae551c43946a70f02596ff9df695
HasanMohsin1 <78063202+HasanMohsin1@users.noreply.github.com>
2022-04-02 15:56:21 -0500

    update commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://867p2id5z4.execute-api.us-east-1.amazonaws.com'
 };
