907d17c6b3b0ed034cbac655e976431b9ecefb5a
HasanMohsin1 <78063202+HasanMohsin1@users.noreply.github.com>
2022-04-08 17:22:18 -0500

    update commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://867p2id5z4.execute-api.us-east-1.amazonaws.com'
+  api_url: 'https://k8tr8m1pe7.execute-api.us-east-1.amazonaws.com'
 };
